6d87e8ff4d1ba5b7019b423a708ddc4f4131ae92
betomoedano <betomoedano@outlook.com>
2025-02-08 20:37:08 -0600

    add expo logo and cool button

--- targets/watch/content.swift
+++ targets/watch/content.swift
@@ -1,19 +1,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  @State private var isPressed = false
+  
+  var body: some View {
+    VStack {
+      Image("expo-icon")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 50, height: 50)
+        .rotationEffect(.degrees(isPressed ? 360 : 0))
+        .animation(.spring(), value: isPressed)
+      
+      Text("Powered by ")
+        .font(.title3)
+      + Text("Expo")
+        .font(.title3)
+        .fontWeight(.bold)
+      
+      Spacer()
+      
+      Button(action: {
+        isPressed.toggle()
+      }) {
+        Text("Spin Logo!")
+      }
     }
+    .padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }

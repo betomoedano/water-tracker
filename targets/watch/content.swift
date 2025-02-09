c1adcb819d26c2d5b8c102f8ad397d91f2bd3426
betomoedano <betomoedano@outlook.com>
2025-02-09 07:55:54 -0600

    add support for handGestureShortcut

--- targets/watch/content.swift
+++ targets/watch/content.swift
@@ -4,29 +4,34 @@ struct ContentView: View {
   @State private var isPressed = false
   
   var body: some View {
-    VStack {
-      Image("expo-icon")
-        .resizable()
-        .scaledToFit()
-        .frame(width: 50, height: 50)
-        .rotationEffect(.degrees(isPressed ? 360 : 0))
-        .animation(.spring(), value: isPressed)
-      
-      Text("Powered by ")
-        .font(.title3)
-      + Text("Expo")
-        .font(.title3)
-        .fontWeight(.bold)
-      
-      Spacer()
-      
-      Button(action: {
-        isPressed.toggle()
-      }) {
-        Text("Spin Logo!")
+    if #available(watchOS 11.0, *) {
+      VStack {
+        Image("expo-icon")
+          .resizable()
+          .scaledToFit()
+          .frame(width: 50, height: 50)
+          .rotationEffect(.degrees(isPressed ? 360 : 0))
+          .animation(.spring(), value: isPressed)
+        
+        Text("Powered by ")
+          .font(.title3)
+        + Text("Expo")
+          .font(.title3)
+          .fontWeight(.bold)
+        
+        Spacer()
+        
+        Button(action: {
+          isPressed.toggle()
+        }) {
+          Text("Spin Logo!")
+        }
+        .handGestureShortcut(.primaryAction, isEnabled: true)
       }
+      .padding()
+    } else {
+      // Fallback on earlier versions
     }
-    .padding()
   }
 }
 
--- targets/watch/expo-target.config.js
+++ targets/watch/expo-target.config.js
@@ -1,8 +1,12 @@
 /** @type {import('@bacons/apple-targets/app.plugin').ConfigFunction} */
-module.exports = config => ({
+module.exports = (config) => ({
   type: "watch",
-  icon: 'https://github.com/expo.png',
-  colors: { $accent: "darkcyan", },
+  icon: "https://github.com/expo.png",
+  colors: { $accent: "darkcyan" },
   deploymentTarget: "9.4",
-  entitlements: { /* Add entitlements */ },
+  entitlements: {
+    /* Add entitlements */
+  },
+  name: "Water Tracker",
+  appleTeamId: "T2A8YY9YDW",
 });
